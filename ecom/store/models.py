647099179b94b3aba1156ddf3cd255e46f7aebbf
Sacheen <sachin.shrestha42@gmail.com>
2024-08-05 21:27:53 +1000

    images

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -33,9 +33,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='uploads/product_images/')
 
     def __str__(self):
         return f"Image for {self.product.name}"
